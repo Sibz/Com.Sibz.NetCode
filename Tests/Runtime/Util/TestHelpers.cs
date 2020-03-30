4f6da676391fe57eeaecef1792c01fd872a5c1d1
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-03-30 22:58:42 +1100

    Added test helper to get instance of class So can now instantiate internal classes for testing providing they can be cast to an interface.

---
+++ Tests/Runtime/Util/TestHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Sibz.NetCode.Tests.Util
+{
+    public class TestHelpers
+    {
+        public static T GetInstanceOf<T>(string className, string nameSpace = null)
+            where T : class =>
+            Activator.CreateInstance(Assembly.GetAssembly(typeof(T)).GetTypes()
+                .First(x => x.Name == className && x.IsClass && !(x.GetInterface(typeof(T).Name) is null) &&
+                            (nameSpace is null || x.Namespace == nameSpace))) as T;
+    }
+}

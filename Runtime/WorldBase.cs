04e53aa154a1c239e872506eae4c418494bee1c1
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-03-31 16:44:54 +1100

    WorldBase: Added null check on options parameter

--- Runtime/WorldBase.cs
+++ Runtime/WorldBase.cs
@@ -13,18 +13,20 @@ using Unity.NetCode;
 namespace Sibz.NetCode
 {
     public abstract class WorldBase<T> : IDisposable
-    where T: ComponentSystemGroup
+        where T : ComponentSystemGroup
     {
         public readonly World World;
 
         protected readonly BeginInitCommandBuffer CommandBuffer;
 
-        protected WorldBase(IWorldOptionsBase options, Func<World, string, World> creationMethod, List<Type> systems = null)
+        protected WorldBase(IWorldOptionsBase options, Func<World, string, World> creationMethod,
+            List<Type> systems = null)
         {
-            if (creationMethod is null)
+            if (creationMethod is null || options is null)
             {
-                throw new ArgumentNullException(nameof(creationMethod));
+                throw new ArgumentNullException(creationMethod is null ? nameof(creationMethod) : nameof(options));
             }
+
             World = creationMethod.Invoke(World.DefaultGameObjectInjectionWorld, options.WorldName);
 
             CommandBuffer = new BeginInitCommandBuffer(World);
@@ -42,6 +44,7 @@ namespace Sibz.NetCode
         protected Entity CreateEventEntity<T>()
             where T : struct, IComponentData =>
             CommandBuffer.Buffer.CreateSingleton<T>();
+
         protected Entity CreateEventEntity<T>(T data)
             where T : struct, IComponentData =>
             CommandBuffer.Buffer.CreateSingleton(data);

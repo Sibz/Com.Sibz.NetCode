27814c19e0722de73b31873a6ad820890ac928cf
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-01 20:32:46 +1100

    Client: Added ClientOptions

---
+++ Packages/Com.Sibz.NetCode.Client/Runtime/Resources/ClientOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Networking.Transport;
+using UnityEngine;
+
+namespace Sibz.NetCode
+{
+    public class ClientOptions : IWorldOptionsBase
+    {
+        public string WorldName { get; set; } = "Client";
+        public List<GameObject> SharedDataPrefabs { get; } = new List<GameObject>();
+        public List<Type> SystemImportAttributes { get; } = new List<Type>();
+        public bool ConnectOnSpawn { get; set; } = false;
+        public int ConnectTimeout { get; set; } = 10;
+        public string Address { get; set; } = "127.0.0.1";
+        public ushort Port { get; set; } = 21650;
+        public NetworkFamily NetworkFamily { get; set; } = NetworkFamily.Ipv4;
+    }
+}

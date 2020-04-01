d1269fc5a4c65a29711d111994c64750897772d6
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-01 20:42:38 +1100

    Added initial ClientWorld constructor and Connect method

--- Packages/Com.Sibz.NetCode.Client/Runtime/ClientWorld.cs
+++ Packages/Com.Sibz.NetCode.Client/Runtime/ClientWorld.cs
@@ -1,11 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Sibz.NetCode.Client;
+using Unity.Entities;
 using Unity.NetCode;
+using Unity.Networking.Transport;
 
 namespace Sibz.NetCode
 {
-    public class ClientWorld : WorldBase<ClientSimulationSystemGroup>
+    public class ClientWorld : WorldBase<ClientSimulationSystemGroup, NetworkStatus>
     {
-        protected ClientWorld(IWorldOptionsBase options) : base(options, ClientServerBootstrap.CreateClientWorld)
+        protected ClientOptions Options { get; }
+
+        protected ClientWorld(ClientOptions options, List<Type> systems = null)
+        : base(options??new ClientOptions(), ClientServerBootstrap.CreateClientWorld, systems.AppendTypesWithAttribute<ClientSystemAttribute>())
         {
+            Options = options ?? new ClientOptions();
+
+            if (Options.ConnectOnSpawn)
+            {
+                Connect();
+            }
+        }
+
+        private void Connect()
+        {
+            NetworkEndPoint endPoint = NetworkEndPoint.Parse(Options.Address, Options.Port, Options.NetworkFamily);
         }
     }
 }

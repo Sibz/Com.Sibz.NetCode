e88475cf79527b7abfe839e1abc830e4c4ff4581
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-01 20:25:43 +1100

    Client: Renamed base class to ClientWorld, no longer abstract

---
+++ Packages/Com.Sibz.NetCode.Client/Runtime/ClientWorld.cs
@@ -0,0 +1,11 @@
+﻿using Unity.NetCode;
+
+namespace Sibz.NetCode.Client
+{
+    public class ClientWorld : WorldBase<ClientSimulationSystemGroup>
+    {
+        protected ClientWorld(IWorldOptionsBase options) : base(options, ClientServerBootstrap.CreateClientWorld)
+        {
+        }
+    }
+}

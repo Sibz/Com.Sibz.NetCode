222c4c6bc1efc9733a9fcad3003999993de3567f
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-03 20:52:29 +1100

    Added util world extensions HookSystem/NetworkStreamReceiveSystem

---
+++ Packages/Com.Sibz.NetCode/Runtime/Resources/Util/Util.WorldExtension.HookSystem.cs
@@ -0,0 +1,10 @@
+﻿using Sibz.NetCode;
+using Unity.Entities;
+
+namespace Sibz
+{
+    public static partial class Util
+    {
+        public static NetCodeHookSystem HookSystem(this World world) => world.GetExistingSystem<NetCodeHookSystem>();
+    }
+}
---
+++ Packages/Com.Sibz.NetCode/Runtime/Resources/Util/Util.WorldExtension.NetworkStreamReceiveSystem.cs
@@ -0,0 +1,11 @@
+﻿using Sibz.NetCode;
+using Unity.Entities;
+using Unity.NetCode;
+
+namespace Sibz
+{
+    public static partial class Util
+    {
+        public static NetworkStreamReceiveSystem NetworkStreamReceiveSystem(this World world) => world.GetExistingSystem<NetworkStreamReceiveSystem>();
+    }
+}

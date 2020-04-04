68d7461f9eedc5cf3360dafd7bdc91c18f7d4126
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-04 20:17:04 +1100

    Created ServerWorld empty implementation files

--- Packages/Com.Sibz.NetCode.Server/Runtime/ServerWorld.cs
+++ Packages/Com.Sibz.NetCode.Server/Runtime/ServerWorld.cs
@@ -13,29 +13,6 @@ namespace Sibz.NetCode
 {
     public class ServerWorld : WorldBase
     {
-        public Action<Entity> ClientConnected;
-        public Action<Entity> ClientDisconnected;
-        protected ServerOptions Options { get; }
-
-        public void Listen()
-        {
-            NetworkEndPoint endPoint = NetworkEndPoint.Parse(Options.Address, Options.Port, Options.NetworkFamily);
-
-            World.GetNetworkStreamReceiveSystem().Listen(endPoint);
-
-            /*NetworkStatus networkStatus = new NetworkStatus
-            {
-                NetworkState = NetworkStreamReceiveSystem.Driver.Listening
-                    ? NetworkState.Listening
-                    : NetworkState.ListenFailed
-            };*/
-
-            /*
-            World.EntityManager.SetComponentData(NetworkStatusEntity, networkStatus);
-
-            World.EnqueueEvent(new NetworkStateChangeEvent {StatusEntity = NetworkStatusEntity});*/
-        }
-
         public ServerWorld(IWorldManager worldManager) : base(worldManager)
         {
         }
---
+++ Packages/Com.Sibz.NetCode.Server/Runtime/ServerWorldManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Sibz.WorldSystemHelpers;
+using Unity.Entities;
+using Unity.NetCode;
+
+namespace Sibz.NetCode
+{
+    public class ServerWorldManager : WorldManagerBase
+    {
+        public ServerWorldManager(IWorldManagerOptions options) : base(options)
+        {
+        }
+
+        protected override World BootStrapCreateWorld(string worldName) =>
+            ClientServerBootstrap.CreateServerWorld(World.DefaultGameObjectInjectionWorld, worldName);
+
+        protected override void InjectSystems(List<Type> systems) =>
+            World.ImportSystemsFromList<ServerSimulationSystemGroup>(systems);
+    }
+}

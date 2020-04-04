141cc40235e14320766a98fa48ea27522543d6a7
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-04 19:49:41 +1100

    Server/Client: Added CallBackProvider interfaces

---
+++ Packages/Com.Sibz.NetCode.Client/Runtime/Resources/IClientWorldCallbackProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sibz.NetCode
+{
+    public interface IClientWorldCallbackProvider
+    {
+        Action Connecting { get; set; }
+        Action Connected { get; set; }
+        Action ConnectionFailed { get; set; }
+        Action Disconnected { get; set; }
+    }
+}
---
+++ Packages/Com.Sibz.NetCode.Server/Runtime/Resources/IServerWorldCallbackProvider.cs
@@ -0,0 +1,11 @@
+﻿﻿using System;
+using Unity.Networking.Transport;
+
+namespace Sibz.NetCode
+{
+    public interface IServerWorldCallbackProvider
+    {
+        Action<NetworkConnection> ClientConnected { get; set; }
+        Action<NetworkConnection> ClientDisconnected { get; set; }
+    }
+}

d716fc161959c31fa335dd0ca08e53d7ae4d44e1
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-05 16:58:03 +1000

    ServerWorldManager: Added more tests

--- Packages/Com.Sibz.NetCode/Tests/Editor/ServerTests/ServerWorldManagerTests.cs
+++ Packages/Com.Sibz.NetCode/Tests/Editor/ServerTests/ServerWorldManagerTests.cs
@@ -48,17 +48,95 @@ namespace Sibz.NetCode.Tests.Server
         public void Listen_ShouldInvokeCallback()
         {
             bool success = false;
+
             void OnListenSuccess()
             {
                 success = true;
             }
+
             wm.CallbackProvider = new MyServerCallbackProvider { ListenSuccess = OnListenSuccess };
             wm.CreateWorld(new List<Type> { typeof(NetCodeEventComponentSystem) });
             wm.Listen(serverOptions);
             Assert.IsTrue(success);
         }
 
-        public class MyServerCallbackProvider: IServerWorldCallbackProvider
+        /*
+        [Test]
+        public void Listen_WhenPortTaken_ShouldFailAndInvokeCallback()
+        {
+            bool success = false;
+
+            void OnListenFailed()
+            {
+                success = true;
+            }
+
+            var wm2 = new ServerWorldManager(new MyWorldManagerOptions());
+            wm2.CreateWorld(new List<Type> { typeof(NetCodeEventComponentSystem) });
+            wm2.Listen(serverOptions);
+            wm.CallbackProvider = new MyServerCallbackProvider { ListenFailed = OnListenFailed };
+            wm.CreateWorld(new List<Type> { typeof(NetCodeEventComponentSystem) });
+            wm.Listen(serverOptions);
+            wm2.Dispose();
+            Assert.IsTrue(success);
+        }
+        */
+
+        [Test]
+        public void Close_WhenWorldNotCreated_ShouldNotThrow()
+        {
+            wm.Close();
+        }
+
+        [Test]
+        public void Close_WhenWorldNotListening_ShouldNotThrow()
+        {
+            wm.CreateWorld(new List<Type> { typeof(NetCodeEventComponentSystem) });
+            wm.Close();
+        }
+
+        [Test]
+        public void Close_WhenListening_ShouldDestroyWorld()
+        {
+            wm.CreateWorld(new List<Type> { typeof(NetCodeEventComponentSystem) });
+            wm.Listen(serverOptions);
+            wm.Close();
+            Assert.IsFalse(wm.World.IsCreated);
+        }
+
+        [Test]
+        public void Close_WhenListening_ShouldSetListeningToFalse()
+        {
+            wm.CreateWorld(new List<Type> { typeof(NetCodeEventComponentSystem) });
+            wm.Listen(serverOptions);
+
+            Assert.IsTrue(wm.IsListening, "Listening should be set to true for this test");
+
+            wm.Close();
+
+            Assert.IsFalse(wm.IsListening);
+        }
+
+        [Test]
+        public void Close_WhenListening_ShouldInvokeCallback()
+        {
+            bool success = false;
+
+            void OnClose()
+            {
+                success = true;
+            }
+
+            wm.CreateWorld(new List<Type> { typeof(NetCodeEventComponentSystem) });
+            wm.Listen(serverOptions);
+            wm.CallbackProvider = new MyServerCallbackProvider { Closed = OnClose };
+            wm.Close();
+
+            Assert.IsTrue(success);
+        }
+
+
+        public class MyServerCallbackProvider : IServerWorldCallbackProvider
         {
             public Action WorldCreated { get; set; }
             public Action WorldDestroyed { get; set; }

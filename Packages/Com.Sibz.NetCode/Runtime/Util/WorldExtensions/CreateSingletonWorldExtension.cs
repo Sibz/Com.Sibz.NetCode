48ad291dec84006b23f605eb81d0c1a6a2ea0dc2
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-05 20:33:44 +1000

    Added CreateSingletonWorldExtension

---
+++ Packages/Com.Sibz.NetCode/Runtime/Util/WorldExtensions/CreateSingletonWorldExtension.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+
+namespace Sibz.NetCode.WorldExtensions
+{
+    public static class CreateSingletonWorldExtension
+    {
+        public static Entity CreateSingleton<T>(this World world, T data)
+            where T : struct, IComponentData
+        {
+            var entity = world.EntityManager.CreateEntity();
+            world.EntityManager.AddComponentData(entity, data);
+            return entity;
+        }
+
+        public static Entity CreateSingleton<T>(this World world)
+            where T : struct, IComponentData =>
+            world.EntityManager.CreateEntity(typeof(T));
+    }
+}

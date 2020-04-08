4170ddbf0ae5b0a4ef7a9ea95d1c0284afdd5402
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-08 11:00:42 +1000

    #2 Added GoInGameRequestSystem

---
+++ Packages/Com.Sibz.NetCode.Client/Runtime/Systems/GoInGameRequestSystem.cs
@@ -0,0 +1,10 @@
+﻿using Sibz.NetCode;
+using Unity.NetCode;
+
+namespace Packages.Com.Sibz.NetCode.Client.Runtime.Systems
+{
+    [ClientSystem]
+    public class GoInGameRequestSystem : RpcCommandRequestSystem<GoInGameRequest>
+    {
+    }
+}

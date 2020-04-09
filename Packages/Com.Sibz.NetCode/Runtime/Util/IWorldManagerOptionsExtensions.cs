4afac9707715b66bb7110419c5f1330982d8dd0a
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-09 18:43:36 +1000

    Added helper for adding systems inside options.

---
+++ Packages/Com.Sibz.NetCode/Runtime/Util/IWorldManagerOptionsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sibz.NetCode;
+
+namespace Sibz
+{
+    public static class WorldManagerOptionsExtensions
+    {
+        public static IWorldManagerOptions GetOptionsWithImportedSystems<T>(this IWorldManagerOptions options)
+        where T: Attribute
+        {
+            options.Systems.AppendTypesWithAttribute<T>();
+            return options;
+        }
+    }
+}

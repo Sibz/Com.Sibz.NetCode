89b165f3bd7d5482e584ab32c7e4fde9ba65e40b
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-09 14:38:29 +1000

    #8 Added initial code

---
+++ Packages/Com.Sibz.NetCode.Server/Runtime/Systems/DisconnectSystem.cs
@@ -0,0 +1,18 @@
+﻿using Sibz.NetCode.Server;
+using Unity.Entities;
+
+namespace Sibz.NetCode
+{
+    public class DisconnectSystem : ComponentSystem
+    {
+        protected override void OnCreate()
+        {
+            RequireSingletonForUpdate<Disconnect>();
+        }
+
+        protected override void OnUpdate()
+        {
+
+        }
+    }
+}
---
+++ Packages/Com.Sibz.NetCode/Tests/Editor/ServerTests/DisconnectSystemTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Sibz.EntityEvents;
+using Unity.Entities;
+
+namespace Sibz.NetCode.Tests.Server
+{
+    public class DisconnectSystemTests
+    {
+        private World world;
+        private MyDisconnectSystem disconnectSystem;
+        private BeginInitializationEntityCommandBufferSystem bufferSystem;
+        private EventComponentSystem eventComponentSystem;
+        private int testCount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            world = new World($"Test_DisconnectSystem_{testCount++}");
+            bufferSystem = world.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+            eventComponentSystem = world.GetOrCreateSystem<EventComponentSystem>();
+            disconnectSystem = world.GetOrCreateSystem<MyDisconnectSystem>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            world.Dispose();
+        }
+
+        [Test]
+        public void WhenSingletonDoesNotExist_ShouldNotRun()
+        {
+            disconnectSystem.Update();
+            Assert.IsFalse(disconnectSystem.DidUpdate);
+        }
+    }
+
+    public class MyDisconnectSystem : DisconnectSystem
+    {
+        public bool DidUpdate;
+        protected override void OnUpdate()
+        {
+            DidUpdate = true;
+            base.OnUpdate();
+        }
+    }
+}

8a0d75594241f80d89cfe1ae50fa9b82c4433bce
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-14 15:11:50 +1000

    #51 Updated DisconnectJob to use NetworkStreamRequestDisconnect Closes#51

--- Packages/Com.Sibz.NetCode.Client/Runtime/Systems/DisconnectSystem.cs
+++ Packages/Com.Sibz.NetCode.Client/Runtime/Systems/DisconnectSystem.cs
@@ -48,24 +48,6 @@ namespace Sibz.NetCode.Client
             return inputDeps;
         }
 
-        public struct DisconnectJob
-        {
-            public EntityCommandBuffer.Concurrent CommandBuffer;
-            public bool EventExists;
-            public Entity Disconnect;
-            public EnqueueEventJobPart<DisconnectedEvent> EnqueueEventJobPart;
 
-            public void Execute(int index, Entity networkStreamEntity)
-            {
-                if (!EventExists)
-                {
-                    EnqueueEventJobPart.Execute();
-                    return;
-                }
-
-                CommandBuffer.DestroyEntity(index, Disconnect);
-                CommandBuffer.AddComponent<NetworkStreamDisconnected>(index, networkStreamEntity);
-            }
-        }
     }
 }

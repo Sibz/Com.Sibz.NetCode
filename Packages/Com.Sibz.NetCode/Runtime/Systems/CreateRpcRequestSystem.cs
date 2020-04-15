f6052ae8d1b0a366cfc0f693e7f11522d33fe7c5
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-15 11:05:04 +1000

    #48 CreateRpc system is now multi connection friendly Defaults to first found network connection if not specified Closes #48

--- Packages/Com.Sibz.NetCode/Runtime/Systems/CreateRpcRequestSystem.cs
+++ Packages/Com.Sibz.NetCode/Runtime/Systems/CreateRpcRequestSystem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.NetCode;
@@ -10,21 +11,40 @@ namespace Sibz.NetCode
     {
         private EntityQuery networkIdEntityQuery;
 
-        public Entity CommandTargetComponentEntity =>
-            networkIdEntityQuery.GetSingletonEntity();
+        public Entity CommandTargetComponentEntity
+        {
+            get
+            {
+                if (networkIdEntityQuery.CalculateEntityCount() == 0)
+                {
+                    throw new InvalidOperationException("No active network connection for Rpc request");
+                }
+
+                Entity result;
+                using (NativeArray<Entity> entities =
+                    networkIdEntityQuery.ToEntityArrayAsync(Allocator.TempJob, out JobHandle jh))
+                {
+                    jh.Complete();
+                    result = entities[0];
+                }
+                return result;
+            }
+        }
+
+
 
         private static Entity GetTargetConnection(World world)
         {
             return world.GetOrCreateSystem<CreateRpcRequestSystem>().CommandTargetComponentEntity;
         }
 
-        public static Entity CreateRpcRequest<T>(World world)
+        public static Entity CreateRpcRequest<T>(World world, Entity targetConnection = default)
             where T : struct, IRpcCommand
         {
-            return CreateRpcRequest<T>(world, default);
+            return CreateRpcRequest<T>(world, default, targetConnection);
         }
 
-        public static Entity CreateRpcRequest<T>(World world, T rpcCommand)
+        public static Entity CreateRpcRequest<T>(World world, T rpcCommand, Entity targetConnection = default)
             where T : struct, IRpcCommand
         {
             if (world is null)
@@ -35,7 +55,23 @@ namespace Sibz.NetCode
             Entity e = world.EntityManager.CreateEntity();
             world.EntityManager.AddComponentData(e, rpcCommand);
             world.EntityManager.AddComponentData(e,
-                new SendRpcCommandRequestComponent { TargetConnection = GetTargetConnection(world) });
+                new SendRpcCommandRequestComponent
+                {
+                    TargetConnection = targetConnection.Equals(Entity.Null) ?
+                        GetTargetConnection(world)
+                        : targetConnection
+                });
+            return e;
+        }
+
+        public static Entity CreateRpcRequest<T>(EntityCommandBuffer buffer, T rpcCommand,
+            Entity targetConnection)
+            where T : struct, IRpcCommand
+        {
+            Entity e = buffer.CreateEntity();
+            buffer.AddComponent(e, rpcCommand);
+            buffer.AddComponent(e,
+                new SendRpcCommandRequestComponent { TargetConnection = targetConnection });
             return e;
         }
 
--- Packages/Com.Sibz.NetCode/Runtime/Util/Util.CreateRpcRequest.cs
+++ Packages/Com.Sibz.NetCode/Runtime/Util/Util.CreateRpcRequest.cs
@@ -6,16 +6,23 @@ namespace Sibz
 {
     public static partial class Util
     {
-        public static Entity CreateRpcRequest<T>(this World world, T data)
+        public static Entity CreateRpcRequest<T>(this World world, T data, Entity targetConnection = default)
             where T : struct, IRpcCommand
         {
-            return CreateRpcRequestSystem.CreateRpcRequest(world, data);
+            return CreateRpcRequestSystem.CreateRpcRequest(world, data,targetConnection);
         }
 
-        public static Entity CreateRpcRequest<T>(this World world)
+        public static Entity CreateRpcRequest<T>(this World world, Entity targetConnection = default)
             where T : struct, IRpcCommand
         {
-            return CreateRpcRequestSystem.CreateRpcRequest<T>(world);
+            return CreateRpcRequestSystem.CreateRpcRequest<T>(world, targetConnection);
+        }
+
+        public static Entity CreateRpcRequest<T>(this EntityCommandBuffer buffer, T data,
+            Entity targetConnection)
+            where T : struct, IRpcCommand
+        {
+            return CreateRpcRequestSystem.CreateRpcRequest(buffer, data, targetConnection);
         }
 
         public static Entity CreateRpcRequest<T>(this EntityCommandBuffer.Concurrent buffer, int index, T data,

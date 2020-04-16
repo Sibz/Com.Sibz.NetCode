09959cf77773cf5b6663d4a11034bbc0907c046e
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-16 13:40:27 +1000

    #37 Added initial files for server client disconnection

---
+++ Packages/Com.Sibz.NetCode.Server/Runtime/Systems/DisconnectClientSystem.cs
@@ -0,0 +1,14 @@
+﻿using Sibz.NetCode;
+using Unity.Entities;
+
+namespace Packages.Com.Sibz.NetCode.Server.Runtime.Systems
+{
+    [ServerSystem]
+    public class DisconnectClientSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Packages/Com.Sibz.NetCode/Tests/Editor/ServerTests/DisconnectClientSystemTests.cs
@@ -0,0 +1,18 @@
+﻿using Packages.Com.Sibz.NetCode.Server.Runtime.Systems;
+
+namespace Sibz.NetCode.Tests.Server
+{
+    public class DisconnectClientSystemTests
+    {
+        public class MyDisconnectClientSystem : DisconnectClientSystem
+        {
+            public bool DidUpdate;
+
+            protected override void OnUpdate()
+            {
+                DidUpdate = true;
+                base.OnUpdate();
+            }
+        }
+    }
+}

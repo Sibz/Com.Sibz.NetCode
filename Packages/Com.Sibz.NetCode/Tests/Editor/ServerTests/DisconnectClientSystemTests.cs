4dabb9aad5aa084922df9c35212b67dde3444d84
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-16 13:48:19 +1000

    #37 Added test setup

--- Packages/Com.Sibz.NetCode/Tests/Editor/ServerTests/DisconnectClientSystemTests.cs
+++ Packages/Com.Sibz.NetCode/Tests/Editor/ServerTests/DisconnectClientSystemTests.cs
@@ -1,9 +1,23 @@
-﻿using Packages.Com.Sibz.NetCode.Server.Runtime.Systems;
+﻿using NUnit.Framework;
+using Packages.Com.Sibz.NetCode.Server.Runtime.Systems;
+using Unity.Entities;
 
 namespace Sibz.NetCode.Tests.Server
 {
     public class DisconnectClientSystemTests
     {
+        private World world;
+        private MyDisconnectClientSystem system;
+        private int testCount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            world = new World($"TestDisconnectClientSystem{testCount++}");
+            world.CreateSystem<DisconnectClientSystem>();
+        }
+
+
         public class MyDisconnectClientSystem : DisconnectClientSystem
         {
             public bool DidUpdate;

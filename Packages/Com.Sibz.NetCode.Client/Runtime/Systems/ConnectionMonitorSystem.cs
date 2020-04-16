a6da383363512821cdf57fa7e8242a0d10f73f0b
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-16 11:57:24 +1000

    #49 Added initial system

---
+++ Packages/Com.Sibz.NetCode.Client/Runtime/Systems/ConnectionMonitorSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using Unity.NetCode;
+
+namespace Packages.Com.Sibz.NetCode.Client.Runtime.Systems
+{
+    public class ConnectionMonitorSystem : SystemBase
+    {
+        private EntityQuery networkStream;
+
+        protected override void OnCreate()
+        {
+            networkStream =
+                EntityManager.CreateEntityQuery(typeof(NetworkIdComponent), typeof(NetworkStreamConnection));
+            RequireForUpdate(networkStream);
+        }
+
+        protected override void OnUpdate()
+        {
+
+        }
+    }
+}
---
+++ Packages/Com.Sibz.NetCode/Tests/Editor/ClientTests/ConnectionMonitorSystemTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Packages.Com.Sibz.NetCode.Client.Runtime.Systems;
+using Sibz.EntityEvents;
+using Unity.Entities;
+using Unity.NetCode;
+
+namespace Sibz.NetCode.Tests.Client
+{
+    public class ConnectionMonitorSystemTests
+    {
+        private World world;
+        private MyConnectionMonitorSystem system;
+        private int testCount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            world = new World($"TestCMS${testCount++}");
+            world.CreateSystem<BeginInitializationEntityCommandBufferSystem>();
+            world.CreateSystem<EventComponentSystem>();
+            system = world.CreateSystem<MyConnectionMonitorSystem>();
+        }
+        [Test]
+        public void WhenConnectionExist_ShouldRun()
+        {
+            world.EntityManager.CreateEntity(typeof(NetworkIdComponent), typeof(NetworkStreamConnection));
+        }
+
+
+        private class MyConnectionMonitorSystem : ConnectionMonitorSystem
+        {
+            public bool DidUpdate = false;
+            protected override void OnUpdate()
+            {
+                DidUpdate = true;
+                base.OnUpdate();
+            }
+        }
+    }
+}

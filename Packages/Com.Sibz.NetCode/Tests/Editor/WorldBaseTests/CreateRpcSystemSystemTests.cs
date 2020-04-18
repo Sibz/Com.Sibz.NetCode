8e57a8e1af8430069ee8f048a89b61a006a311dd
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-18 16:16:22 +1000

    #54 Created CreateRpcSystemSystemTests with some initial tests

---
+++ Packages/Com.Sibz.NetCode/Tests/Editor/WorldBaseTests/CreateRpcSystemSystemTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using NUnit.Framework;
+using Sibz.NetCode.WorldExtensions;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.NetCode;
+
+namespace Sibz.NetCode.Tests.Base
+{
+    public class CreateRpcSystemSystemTests
+    {
+        private World world;
+        private MyCreateRpcRequestSystem system;
+        private int testCount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            world = new World($"Test{nameof(CreateRpcRequestSystem)}{testCount++}");
+            system = world.CreateSystem<MyCreateRpcRequestSystem>();
+        }
+
+        [Test]
+        public void WhenUpdateIsCalled_ShouldNotUpdate()
+        {
+            system.Update();
+            Assert.IsFalse(system.DidUpdate);
+        }
+
+        [Test]
+        public void WhenUpdateIfForced_ShouldThrow()
+        {
+            Assert.Catch<InvalidOperationException>(() => system.DoUpdate());
+        }
+
+        [Test]
+        public void GetCommandTargetComponentEntity_WhenNoNetworkConnection_ShouldThrow()
+        {
+            Assert.Catch<InvalidOperationException>(() =>
+            {
+                Entity x = system.CommandTargetComponentEntity;
+            });
+        }
+
+        [Test]
+        public void CreateRpcRequest_WhenWorldIsNull_ShouldThrow()
+        {
+            Assert.Catch<ArgumentNullException>(() =>
+                CreateRpcRequestSystem.CreateRpcRequest(null, new GoInGameRequest()));
+        }
+
+        [Test]
+        public void GetCommandTargetComponentEntity_WhenNetworkConnection_GetEntity()
+        {
+            world.CreateSingleton<NetworkIdComponent>();
+            Entity x = system.CommandTargetComponentEntity;
+            Assert.IsFalse(x.Equals(Entity.Null));
+        }
+
+
+        private class MyCreateRpcRequestSystem : CreateRpcRequestSystem
+        {
+            public bool DidUpdate;
+
+            public void DoUpdate()
+            {
+                OnUpdate(default);
+            }
+
+            protected override JobHandle OnUpdate(JobHandle inputDeps)
+            {
+                DidUpdate = true;
+                return base.OnUpdate(inputDeps);
+            }
+        }
+    }
+}

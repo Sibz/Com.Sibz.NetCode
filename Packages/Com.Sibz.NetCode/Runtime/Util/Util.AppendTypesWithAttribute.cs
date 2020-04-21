321ff4b90ec85fe0f70637451ee07d61c79084fc
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-21 10:59:01 +1000

    #86 Now importing ghost systems Closes #86

--- Packages/Com.Sibz.NetCode/Runtime/Util/Util.AppendTypesWithAttribute.cs
+++ Packages/Com.Sibz.NetCode/Runtime/Util/Util.AppendTypesWithAttribute.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Unity.Entities;
 
 namespace Sibz
 {
@@ -19,6 +20,37 @@ namespace Sibz
 
             return types;
         }
+
+        public static List<Type> AppendTypesWithUpdateInGroupAttribute(this List<Type> types, Type groupType)
+        {
+            types = types ?? new List<Type>();
+
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (Assembly a in assemblies)
+            {
+                types.AddRange(a.GetTypes().Where(x =>
+                    x.GetCustomAttribute<UpdateInGroupAttribute>() is UpdateInGroupAttribute att
+                    && att.GroupType == groupType));
+            }
+
+            return types;
+        }
+        public static List<Type> AppendTypesThatAreSubclassOf(this List<Type> types, string baseTypeName)
+        {
+            types = types ?? new List<Type>();
+
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (Assembly a in assemblies)
+            {
+                types.AddRange(a.GetTypes().Where(x =>
+                    x.BaseType != null
+                    && x.BaseType.Name.StartsWith(baseTypeName)));
+            }
+
+            return types;
+        }
+
+
 
         public static List<Type> AppendTypesWithAttribute<T>(this List<Type> types)
             where T : Attribute
---
+++ Packages/Com.Sibz.NetCode/Runtime/Util/WorldExtensions/ImportGhostSystemsWorldExtension.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Sibz.WorldSystemHelpers;
+using Unity.Entities;
+using Unity.NetCode;
+
+namespace Sibz.NetCode.WorldExtensions
+{
+    public static class ImportGhostSystemsWorldExtension
+    {
+        private const string WrongWorldError = "Can only import ghost systems into client or server NetCode worlds";
+        private const string GhostSendSystemName = "GhostSendSystem";
+        private const string GhostReceiveSystemName = "GhostReceiveSystem";
+        private const string DefaultGhostSpawnSystemName = "DefaultGhostSpawnSystem";
+
+        public static void ImportGhostSystems(this World world)
+        {
+            if (world.GetExistingSystem<ClientSimulationSystemGroup>() is ClientSimulationSystemGroup)
+            {
+                ImportClientSystems(world);
+            } else if (world.GetExistingSystem<ServerSimulationSystemGroup>() is ServerSimulationSystemGroup)
+            {
+                ImportServerSystems(world);
+            }
+            else
+            {
+                throw new InvalidOperationException(WrongWorldError);
+            }
+        }
+
+        private static void ImportServerSystems(World world)
+        {
+            List<Type> systems = new List<Type>();
+            systems.AppendTypesThatAreSubclassOf(GhostSendSystemName);
+            RemoveSystemsThatAreAlreadyInWorld(world, systems);
+            world.ImportSystemsFromList<ServerSimulationSystemGroup>(systems);
+        }
+
+        private static void ImportClientSystems(World world)
+        {
+            // GhostReceiveSystem, DefaultGhostSpawnSystem
+            //[UpdateInGroup(typeof(GhostUpdateSystemGroup))]
+            List<Type> systems = new List<Type>();
+            systems.AppendTypesThatAreSubclassOf(GhostReceiveSystemName);
+            systems.AppendTypesThatAreSubclassOf(DefaultGhostSpawnSystemName);
+            systems.AppendTypesWithUpdateInGroupAttribute(typeof(GhostUpdateSystemGroup));
+            RemoveSystemsThatAreAlreadyInWorld(world, systems);
+            world.ImportSystemsFromList<ClientSimulationSystemGroup>(systems);
+        }
+
+        private static void RemoveSystemsThatAreAlreadyInWorld(World world, ICollection<Type> systems)
+        {
+            foreach (ComponentSystemBase worldSystem in world.Systems)
+            {
+                if (systems.Contains(worldSystem.GetType()))
+                {
+                    systems.Remove(worldSystem.GetType());
+                }
+            }
+        }
+    }
+}
